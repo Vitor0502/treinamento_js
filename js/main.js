bbd660157fde1bad54fb73e1bc3d892c7dcf502a
Vitor0502 <vitorgames10080@gmail.com>
2022-04-24 00:55:43 -0300

    Teste com eventos JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-alert('Teste');
+// Trabalhando com datas
+
+/* var dat = new Date();
+alert(`Minutos atuais: ${dat.getMinutes()}`); */
+
+function clicka() {
+    document.getElementById("thanks").innerHTML = "<b>Você clicou</b>";
+}
+
+function redirect() {
+    window.open("https://uepb.edu.br/");
+}
+
+function switchText(el) {
+    el.innerHTML = "Mouse passado, texto diferenciado!";
+}
+
+function switchBack(el) {
+    el.innerHTML = "Passe o mouse aqui!";
+}
+
+function getValue(el) {
+    console.log(el.value);
+}
